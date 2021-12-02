7a8785b653b7f265cff2e3d8f83737697e2ffe76
raduboianradcom <radu.boian.radcom@gmail.com>
2021-12-02 18:33:55 +0200

    newmainl

--- run_tasks.py
+++ run_tasks.py
@@ -11,3 +11,4 @@ if __name__ == "__main__":
     # now the task should be finished and ready method will return True
     print("Task finished? ", result.ready())
     print("Important e c asta trebuie sa ajunga din main in dev ")
+    print("Impoanother one")
